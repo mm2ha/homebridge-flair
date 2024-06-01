a643f3c5fe4e9d81258b55c964c5282e8e0fdf90
marko miklo <marko.miklo@thetradedesk.com>
2024-06-01 16:26:30 -0600

    Changing plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Flair AC';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-flair';
+export const PLUGIN_NAME = '@mm2ha/homebridge-flair';
